7df75269deaf5006334c84589d18b4ea75155c21
Simon Papa <>
2023-07-17 08:56:51 +1000

    Ex3 Complete

--- ex3.py
+++ ex3.py
@@ -4,18 +4,21 @@
 
 # 1. Print out your favourite food 42 times using * operator.
 
-# INSERT YOUR CODE HERE
+print("Mum's Pasta" * 42)
 
 # 2. Insert space between each output and print it out again.
 
-# INSERT YOUR CODE HERE
+print(("Mum's Pasta" + " ") * 41 + "Mum's Pasta")
 
 # 3. Save your favourite food into a variable and print it out 42 times
 
-# INSERT YOUR CODE HERE
+favourite_food: str = "Mum's Pasta"
+print(favourite_food*42)
 
 # 4. My favourite food is "sushi", save it into a variable and using
 #    fast swapping operation change it with your variable.
 #    Now your favourite food should be "sushi" and mine will be yours.#
 
-# INSERT YOUR CODE HERE
+rc_favourite_food: str = "sushi"
+favourite_food, rc_favourite_food = rc_favourite_food, favourite_food
+print(favourite_food, " ", rc_favourite_food)
