96c6c9f10e0b04277ca20540e349d944cdaebac4
Simon Papa <>
2023-07-17 08:32:48 +1000

    Ex2 Complete

--- ex2.py
+++ ex2.py
@@ -12,18 +12,22 @@
 
 # 1. Make a python list of the 5 items above and print it out.
 
-# INSERT YOUR CODE HERE
+list_shopping: list = ["Apples", "Milk", "Bread", "Chicken", "Pasta"]
+print(list_shopping)
 
 # 2. Use python as your calculator and print out the total cost of
 #    all items on shopping list.
 
-# INSERT YOUR CODE HERE
+list_shopping_total_cost: float = 2.99 + 1.79 + 3.49 + 6.99 + 2.49
+print(list_shopping_total_cost)
 
 # 3. I have decided that we need 5 cartons of milk, can you recalculate
 #    it and print it out again?
 
-# INSERT YOUR CODE HERE
+list_shopping_total_cost = 2.99 + (1.79*5) + 3.49 + 6.99 + 2.49
+print(list_shopping_total_cost)
 
 # 4. Print out every item of your shopping list on a new line.
 
-# INSERT YOUR CODE HERE
+for x in list_shopping:
+    print(x)
