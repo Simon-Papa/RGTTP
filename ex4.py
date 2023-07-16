2ea11eeda2e849217b47c9eb0765c57ca2a606d3
Simon Papa <>
2023-07-17 09:51:48 +1000

    Ex4 Completed

--- ex4.py
+++ ex4.py
@@ -5,34 +5,57 @@
 # 1. Create a new list with the following values:
 #    Apple, Milk, Bread, Chicken and Pasta
 
-# INSERT YOUR CODE HERE
+list_food: list = ["Apple", "Milk", "Bread", "Chicken", "Pasta"]
 
 # 2. Write a check to see if Orange is in the shopping list or not.
 
-# INSERT YOUR CODE HERE
+if "Orange" in list_food:
+    print("Orange made the list.")
+else:
+    print("Orange didn't make the cut.")
 
 # 3. Write a function "in_shopping_list" that takes a item such as
 #    orange, and returns True or False.
 #    Depending whether the item is in the list or not.
 
-# INSERT YOUR CODE HERE
+
+def in_shopping_list(food: str) -> bool:
+    if food in list_food:
+        return True
+    else:
+        return False
 
 # 4. Write a function "show_shopping_list" that will return a
 #    shopping list and print it out on the screen.
 
-# INSERT YOUR CODE HERE
+
+def show_shopping_list() -> list:
+    return list_food
+
+
+for x in list_food:
+    print(x)
+
 
 # 5. Create a list of the following values: 2.99 1.79 3.49 6.99 2.49
 
-# INSERT YOUR CODE HERE
+list_prices: list = [2.99, 1.79, 3.49, 6.99, 2.49]
 
 # 6. Create a function price_checker, and pass this list as
 #    an argument and let the function return the cheapest price.#
 
-# INSERT YOUR CODE HERE
+
+def price_checker(y: list) -> float:
+    return min(y)
 
 # 7. Write another function show_price, that will call your
 #    price_checker function and uses the result to
 #    print the cheapest price.
 
-# INSERT YOUR CODE HERE
+
+def show_price():
+    lowest_price = price_checker(list_prices)
+    print(lowest_price)
+
+
+show_price()
