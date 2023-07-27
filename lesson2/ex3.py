80bc21824b9506f6ae7006475cd9c04df7ab1dac
Simon Papa <>
2023-07-27 10:55:09 +1000

    First Pass

--- lesson2/ex3.py
+++ lesson2/ex3.py
@@ -6,7 +6,10 @@
 #    name, age, and occupation of a person.
 #    Then, print the sentence using the elements with .format()
 
-# INSERT YOUR CODE HERE
+biography: list = ["Simon Papa", "39", "Technical Support Engineer"]
+
+print("Hi, My name is {0}, I am {1} years old and work as a {2}"
+      .format(*biography))
 
 # 2. The dictionary should contain keys such as
 #    'title', 'author', and 'publication_year'.
@@ -14,7 +17,16 @@
 #    Example:
 #    "The guidebook [title] by [author] was published in [publication_year]."
 
-# INSERT YOUR CODE HERE
+bibliography: dict = {"title": "The Cardinal of the Kremlin",
+                      "author": "Tom Clancy",
+                      "publication_year": 1988}
+
+print("The book {title} by {author} was published in\
+ {publication_year}".format(**bibliography))
+
+# I can't seem to get positional arguments working with a dict, might need
+# a review session
+
 
 # 3. The dictionary should hold details about a spaceship, such as
 #    'name', 'type', and 'purpose'.
@@ -22,4 +34,9 @@
 #    Example:
 #    "The spaceship is called the [name]. It is a [type] used for [purpose]."
 
-# INSERT YOUR CODE HERE
+spaceship: dict = {"name": "Odyssey",
+                   "type": "BC-304 Daedalus Class Battlecruiser",
+                   "purpose": "planetary defence and exploration"}
+
+print("The spaceship is called the {name}. It is a {type} used for\
+ {purpose}.".format(**spaceship))
