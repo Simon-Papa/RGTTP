ce0702a1e2b41b731ef31ed449695caec910a7de
Simon Papa <>
2023-07-27 10:55:29 +1000

    First Pass

--- lesson2/ex4.py
+++ lesson2/ex4.py
@@ -5,12 +5,25 @@
 # 1. Print the formatted keys and values of the dictionary.
 #    versions: dict[str, int] = {'Stein': 15, 'Train': 16, 'Wallaby': 17}
 
-# INSERT YOUR CODE HERE
+versions: dict[str, int] = {'Stein': 15, 'Train': 16, 'Wallaby': 17}
+
+for x in versions:
+    print("{:<10}".format(x), "{}".format(versions[x]))
 
 # 2. Print {} around the version numbers.
+
+for x in versions:
+    print("{:<10}".format(x), "{{{}}}".format(versions[x]))
 
 # INSERT YOUR CODE HERE
 
 # 3. Print numbers in decimal, byte and hexadecimal form.
 
 # INSERT YOUR CODE HERE
+for x in versions:
+    print("{:<10}".format(x), "{:>6b} binary".format(versions[x]))
+    print("{:<10}".format(x), "{:>6o} octal/byte".format(versions[x]))
+    print("{:<10}".format(x), "{:>6} decimal".format(versions[x]))
+    print("{:<10}".format(x), "{:>6x} hex".format(versions[x]))
+
+# Wasn't sure about byte vs binary so I did both
