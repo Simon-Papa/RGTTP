dbc75aaea9f145717a617800dd315e0dcd073511
Simon Papa <>
2023-08-24 15:43:37 +1000

    First Pass

--- lesson4/ex4.py
+++ lesson4/ex4.py
@@ -10,3 +10,18 @@
 #    {"category": "vegetable", "name": "carrot"}
 #
 #    Write a for loop to print out the fruits and vegetables.
+data = [{"category": "fruit", "name": "apple"},
+        {"category": "fruit", "name": "banana"},
+        {"category": "fruit", "name": "orange"},
+        {"category": "vegetable", "name": "carrot"}]
+
+shopping_list: dict[str, list[str]] = {}
+
+for obj in data:
+    category = obj["category"]
+    if category in shopping_list:
+        shopping_list[category].append(obj["name"])
+    else:
+        shopping_list[category] = [obj["name"]]
+
+print(shopping_list)
