2820c74da757a4b3a3147b06621352dda4c73249
Simon Papa <>
2023-08-24 12:18:45 +1000

    First Pass

--- lesson4/ex2.py
+++ lesson4/ex2.py
@@ -7,7 +7,13 @@
 # orange: 2
 # banana: 3
 
-# INSERT YOUR CODE HERE
+print("Lesson 4, Ex 2.1\n"
+      "Fruits dictionary created\n")
+
+fruits: dict[str, int] = {"apple": 10,
+                          "orange": 2,
+                          "banana": 3}
+
 
 # 2. Iterate over fruits in fruits dictionary using for loop,
 #    print using f-strings:
@@ -15,25 +21,61 @@
 #    orange: 2
 #    banana: 3
 
-# INSERT YOUR CODE HERE
+for keys, values in fruits.items():
+    print("{:<8}".format(keys), ":", "{:>3}".format(values))
+print("\n")
 
 # 2. Iterate over the keys in 'fruits' dictionary
 
-# INSERT YOUR CODE HERE
+print("Lesson 4, Ex 2.2\n"
+      "Iterate over the fruits in the dictionary using a 'for' loop, print\n"
+      "using f-strings\n")
+
+for key in fruits.keys():
+    print(f"{key}")
+print("\n")
+
 
 # 3. Iterate over the values in dictionary
 
-# INSERT YOUR CODE HERE
+print("Lesson 4, Ex 2.3\n"
+      "Iterate over the values in the dictionary using a 'for' loop, print\n"
+      "using f-strings\n")
+
+for value in fruits.values():
+    print(f"{value}")
+print("\n")
+
 
 # 4. Access value banana using .get() method
 
-# INSERT YOUR CODE HERE
+print("Lesson 4, Ex 2.4\n"
+      "Access the value of 'banana' using .get\n")
+
+print(fruits.get("banana"))
+print("\n")
 
 # 5. Access value mandarin using .get() method,
 #    if 'mandarin' doesn't exist, return 0
 
-# INSERT YOUR CODE HERE
+print("Lesson 4, Ex 2.5\n"
+      "Access the value of 'mandarin' using .get. If 'mandarin isn't in the\n"
+      "list, return 0\n")
+
+if fruits.get("mandarin") is None:
+    print(0)
+else:
+    print(fruits.get("mandrin"))
+
+print("\n")
 
 # 6. Remove all items from the dictionary
 
-# INSERT YOUR CODE HERE
+print("Lesson 4, Ex 2.6\n"
+      "Remove all items from the Dictionary\n")
+
+fruits.clear()
+
+for keys, values in fruits.items():
+    print("{:<8}".format(keys), ":", "{:>3}".format(values))
+print("\n")
